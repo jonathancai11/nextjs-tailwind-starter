78f026b6d6074eae53272b20e851fe1f0ddbb657
Jonathan Cai <caimjonathan@gmail.com>
2020-11-26 19:15:20 -0800

    complete landing page redesign using free tailwindui components

--- components/header.js
+++ components/header.js
@@ -1,58 +1,67 @@
-import Link from "next/link";
-import { useState } from "react";
+export default function Header() {
+    return (
+        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+            <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
+                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                    <div className="flex items-center justify-between w-full md:w-auto">
+                        
+                        <a href="/">
+                            <span className="sr-only">Workflow</span>
+                            <img className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" />
+                        </a>
 
-function Header() {
-  const [isExpanded, toggleExpansion] = useState(false);
 
-  return (
-    <header className="bg-teal-500">
-      <div className="flex flex-wrap items-center justify-between max-w-4xl p-4 mx-auto md:flex-no-wrap md:p-8">
-        <div className="flex items-center">
-          <img
-            src="tailwind-logo.svg"
-            className="w-10 mr-3 text-white"
-          />
+                        {/* <div className="-mr-2 flex items-center md:hidden">
+                            <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" id="main-menu" aria-haspopup="true">
+                                <span className="sr-only">Open main menu</span>
+                                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+                                </svg>
+                            </button>
+                        </div> */}
 
-          <Link href="/">
-            <a className="text-xl font-bold text-white">
-              Next.js Starter Tailwind
-            </a>
-          </Link>
+
+                    </div>
+                </div>
+                {/* <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                    <a href="/" className="font-medium text-gray-500 hover:text-gray-900">About</a>
+                    <a href="/#features" className="font-medium text-gray-500 hover:text-gray-900">Features</a>
+                    <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500 float-right">Log in</a>
+                </div> */}
+            </nav>
+
+
+            {/* Dropdown menu */}
+            {/* <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                    <div className="px-5 pt-4 flex items-center justify-between">
+                        <div>
+                            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
+                        </div>
+                        <div className="-mr-2">
+                            <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                <span className="sr-only">Close main menu</span>
+                                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    <div role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
+                        <div className="px-2 pt-2 pb-3 space-y-1" role="none">
+                            <a href="/" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Product</a>
+                            <a href="/" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Features</a>
+                            <a href="/" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Marketplace</a>
+                            <a href="/" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Company</a>
+                        </div>
+                        <div role="none">
+                            <a href="/" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100" role="menuitem">Log in</a>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+
+
         </div>
-
-        <button
-          className="flex items-center block px-3 py-2 text-white border border-white rounded md:hidden"
-          onClick={() => toggleExpansion(!isExpanded)}
-        >
-          <svg
-            className="w-3 h-3 fill-current"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-
-        <ul
-          className={`${
-            isExpanded ? `block` : `hidden`
-          } md:flex flex-col md:flex-row md:items-center md:justify-center text-sm w-full md:w-auto`}
-        >
-          {[
-            { title: "Home", route: "/" },
-            { title: "About", route: "/about" }
-          ].map(navigationItem => (
-            <li className="mt-3 md:mt-0 md:ml-6" key={navigationItem.title}>
-              <Link href={navigationItem.route}>
-                <a className="block text-white">{navigationItem.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </header>
-  );
+    )
 }
-
-export default Header;
--- components/layout.js
+++ components/layout.js
@@ -5,9 +5,9 @@ function Layout(props) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 w-full max-w-4xl p-4 mx-auto md:px-8 md:py-16">
+      <div className="sm:mx-12 md:mx-32 lg:mx-56 xl:mx-72 mx-2"> 
         {props.children}
-      </main>
+      </div>
       <Footer />
     </div>
   );

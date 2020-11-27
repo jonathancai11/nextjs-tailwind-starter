b9a1b604b4dd811f02e1c3dab453c7727b838d5a
Jonathan Cai <caimjonathan@gmail.com>
2020-11-26 21:11:58 -0800

    remove public files and style hero

--- components/hero.js
+++ components/hero.js
@@ -24,7 +24,7 @@ export default function Hero(props) {
 
                     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         <div className="sm:text-center">
-                            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                            <h1 className="text-4xl leading-tight tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                                 <span className="block xl:inline">Data to enrich your</span> {" "}
                                 <span className="block text-indigo-600 xl:inline">online business</span>
                             </h1>

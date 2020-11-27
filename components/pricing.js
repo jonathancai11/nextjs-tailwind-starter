bfa36848967ff0fcde588dd4439ff41398dd1745
Jonathan Cai <caimjonathan@gmail.com>
2020-11-27 15:27:03 -0800

    remove borders from pricing section

--- components/pricing.js
+++ components/pricing.js
@@ -28,14 +28,14 @@ export default function Pricing() {
                     className="bg-white w-4/5 lg:w-custom mb-10 lg:px-4 px-6 py-10 text-center text-gray-900 rounded-lg bg-gray-200"
                 >
                     <h5 className="font-bold text-base">Basic</h5>
-                    <h2 className="pb-4 flex justify-center font-bold border-b border-gray-200">
+                    <h2 className="pb-4 flex justify-center font-bold">
                         <span className="text-3xl mt-6 mr-1">$</span
                         ><span className="text-6xl"> 199.99</span>
                     </h2>
                     <ul className="text-sm font-bold">
-                        <li className="pt-4 pb-4 border-b border-gray-200">500 GB Storage</li>
-                        <li className="pt-3 pb-4 border-b border-gray-200">2 Users Allowed</li>
-                        <li className="pt-4 pb-4 border-b border-gray-200">Send up to 3 GB</li>
+                        <li className="pt-4 pb-4">500 GB Storage</li>
+                        <li className="pt-3 pb-4">2 Users Allowed</li>
+                        <li className="pt-4 pb-4">Send up to 3 GB</li>
                     </ul>
                     <button
                         className=" uppercase text-center text-sm mt-12 xl:px-24 px-12 sm:px-16 py-2 font-bold text-primary-very-light rounded-md bg-indigo-600 text-white"
@@ -48,15 +48,15 @@ export default function Pricing() {
                 >
                     <h5 className="font-bold text-base ">Professional</h5>
                     <h2
-                        className="font-bold pb-4 mt-2 border-b border-gray-100 flex justify-center"
+                        className="font-bold pb-4 mt-2 flex justify-center"
                     >
                         <span className="text-3xl mt-6 mr-1">$</span
                         ><span className="text-6xl "> 249.99</span>
                     </h2>
                     <ul className=" text-sm font-bold">
-                        <li className="pt-4 pb-4 border-b border-gray-200">1 TB Storage</li>
-                        <li className="pt-4 pb-4 border-b border-gray-200">5 Users Allowed</li>
-                        <li className="pt-4 pb-4 border-b border-gray-200">Send up to 10 GB</li>
+                        <li className="pt-4 pb-4">1 TB Storage</li>
+                        <li className="pt-4 pb-4">5 Users Allowed</li>
+                        <li className="pt-4 pb-4">Send up to 10 GB</li>
                     </ul>
                     <button
                         className="uppercase text-center text-sm mt-10 xl:px-24 px-12 sm:px-16 py-2 rounded-md font-bold bg-primary-very-light text-indigo-600 bg-white"
@@ -68,14 +68,14 @@ export default function Pricing() {
                     className="bg-white w-4/5 lg:w-custom mb-10 lg:px-4 px-6 py-10 text-center text-gray-900 rounded-lg bg-gray-200"
                 >
                     <h5 className="font-bold text-base">Master</h5>
-                    <h2 className="flex justify-center pb-4 font-bold border-b border-gray-200">
+                    <h2 className="flex justify-center pb-4 font-bold">
                         <span className="text-3xl mt-6 mr-1">$</span
                         ><span className="text-6xl">399.99</span>
                     </h2>
                     <ul className="text-sm font-bold">
-                        <li className="pt-4 pb-4 border-b border-gray-200">2 TB Storage</li>
-                        <li className="pt-4 pb-4 border-b border-gray-200">10 Users Allowed</li>
-                        <li className="pt-4 pb-4 border-b border-gray-200">Send up to 20 GB</li>
+                        <li className="pt-4 pb-4">2 TB Storage</li>
+                        <li className="pt-4 pb-4">10 Users Allowed</li>
+                        <li className="pt-4 pb-4">Send up to 20 GB</li>
                     </ul>
                     <button
                         className="uppercase text-center text-sm mt-12 xl:px-24 px-12 sm:px-16 py-2 rounded-md font-bold text-primary-very-light bg-indigo-600 text-white"

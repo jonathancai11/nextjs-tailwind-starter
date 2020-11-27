2a4f25f2b52d43c6e0dc09c0f95c2239c61f9d50
Jonathan Cai <caimjonathan@gmail.com>
2020-11-26 21:37:55 -0800

    hero padding

--- components/hero.js
+++ components/hero.js
@@ -1,9 +1,9 @@
-export default function Hero(props) {
+export default function Hero() {
     return (
         <div className="relative bg-white overflow-hidden">
             <div className="max-w-7xl mx-auto">
 
-                <div className="relative z-10 mx-auto pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+                <div className="relative z-10 mx-auto pb-8 bg-white sm:pb-16 md:pb-12 lg:max-w-2xl lg:w-full lg:pb-20 xl:pb-28">
                     {/* <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2" fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none" aria-hidden="true">
                         <polygon points="50,0 100,0 50,100 0,100" />
                     </svg> */}
